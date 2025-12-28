efc2c3e0fcb857851674befd97d9855f6170880d
hiathembahe-cell <hiathembahe@gmail.com>
2025-12-28 11:22:20 +0400

    Modify db.js for new connection string
    Updated database connection string and import statement.

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
-import pkg from "pg";
-const { Pool } = pkg;
+import pg from "pg";
+
+const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  connectionString: "postgresql://am_commercial_db_user:sBMjwfC9sqw4tP9vi5k0G7SnidsrKYPh@dpg-d58biner433s73f62vhg-a.virginia-postgres.render.com/am_commercial_db",
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
+

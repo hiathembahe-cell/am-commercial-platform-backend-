1e1e0e983fa67c8fb6c6aa7605f8255d181f0312
hiathembahe-cell <hiathembahe@gmail.com>
2025-12-28 08:55:40 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import cors from "cors";
+import uploadRoutes from "./routes/upload.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+
+app.use("/api/upload", uploadRoutes);
 
 app.get("/", (req, res) => {
   res.send("A&M Backend Running");
@@ -9,3 +14,4 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
